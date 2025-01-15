ca773c81b680a2ba53b3eddcfa913b6e5f62982e
Garcia003 <tecnologia48hd3@gmail.com>
2025-01-15 10:13:25 -0500

    modo oscuro nice

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,29 @@
+const darkMode = document.querySelector('#switch');
 
-document.getElementById('switch').addEventListener('change', function() {
-    if (this.checked) {
-        document.body.classList.add('dark-mode');
-        document.querySelector('.sidebar-sticky').classList.add('dark-mode');
-        document.querySelector('.content').classList.add('dark-mode');
+const dark = () => {
+    document.body.classList.add('dark-mode');
+    document.querySelector('.sidebar-sticky').classList.add('dark-mode');
+    document.querySelector('.content').classList.add('dark-mode');
+    document.querySelector('#tbodyTable').classList.add('text-light');
+}
+
+darkMode.addEventListener('click', () => {
+    if (darkMode.checked) {
+        dark();
+        window.localStorage.setItem('darkmode', 'true');
     } else {
         document.body.classList.remove('dark-mode');
         document.querySelector('.sidebar-sticky').classList.remove('dark-mode');
         document.querySelector('.content').classList.remove('dark-mode');
+        document.querySelector('#tbodyTable').classList.remove('text-light');
+        window.localStorage.removeItem('darkmode');
     }
-});
+})
+
+$(function () {
+  const initDarkMode = window.localStorage.getItem('darkmode');
+    if (initDarkMode === 'true') {
+        darkMode.checked = true;
+        dark();
+    }  
+})
